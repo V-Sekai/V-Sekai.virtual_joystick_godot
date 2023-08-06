c3622abb6bd0d8458db176c273b4c084e6e9f79f
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2023-08-06 13:59:56 -0700

    Remove the arrows on the tip.

--- test/player.gd
+++ test/player.gd
@@ -8,14 +8,14 @@ extends Sprite3D
 var move_vector := Vector3.ZERO
 
 func _process(delta: float) -> void:
-    # Movement using Input functions:
-    move_vector = Vector3.ZERO
-    move_vector.x = Input.get_axis("ui_left", "ui_right")
-    move_vector.y = Input.get_axis("ui_up", "ui_down")
-    move_vector.z = Input.get_axis("ui_front", "ui_back")
-    transform.origin += move_vector * speed * delta
-    
-    # Rotation:
-    if joystick_right and joystick_right.is_pressed:
-        var angle = joystick_right.output.angle()
-        rotation_degrees = Vector3(0, rad_to_deg(angle), 0)
+	# Movement using Input functions:
+	move_vector = Vector3.ZERO
+	move_vector.x = Input.get_axis("ui_left", "ui_right")
+	move_vector.y = Input.get_axis("ui_up", "ui_down")
+	move_vector.z = Input.get_axis("ui_front", "ui_back")
+	transform.origin += move_vector * speed * delta
+	
+	# Rotation:
+	if joystick_right and joystick_right.is_pressed:
+		var angle = joystick_right.output.angle()
+		rotation_degrees = Vector3(0, rad_to_deg(angle), 0)
